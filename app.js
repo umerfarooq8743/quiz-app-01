db98e87830f13549fe612be3be407bcdc5c563e9
Umer Farooq <64475488+umerfarooq8743@users.noreply.github.com>
2022-12-25 22:35:27 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+function check(){
+    var score = 0;
+    var rtans1=document.getElementById("q1a1");
+    var q1ans2=document.getElementById("q1a2");
+    var q1ans3=document.getElementById("q1a3");
+    var q1ans4=document.getElementById("q1a4");
+    if (rtans1.checked== true){
+    score++
+        alert("Question:1 Answer Is Correct")
+}
+else{
+    alert("Question:1 Answer Is InCorrect")
+}
+var rtans1=document.getElementById("q2a4");
+    var q1ans2=document.getElementById("q2a1");
+    var q1ans3=document.getElementById("q2a2");
+    var q1ans4=document.getElementById("q2a3");
+    if (rtans1.checked== true){
+    score++
+        alert("Question:2 Answer Is Correct")
+}
+else{
+    alert("Question:2 Answer Is InCorrect")
+}
+var rtans1=document.getElementById("q3a1");
+    var q1ans2=document.getElementById("q3a2");
+    var q1ans3=document.getElementById("q3a3");
+    var q1ans4=document.getElementById("q3a4");
+    if (rtans1.checked== true){
+    score++
+        alert("Question:3 Answer Is Correct")
+}
+else{
+    alert("Question:3 Answer Is InCorrect")
+}
+var rtans1=document.getElementById("q4a1");
+    var q3ans2=document.getElementById("q4a2");
+    var q4ans3=document.getElementById("q4a3");
+    var q4ans4=document.getElementById("q4a4");
+    if (rtans1.checked== true){
+    score++
+        alert("Question:4 Answer Is Correct")
+}
+else{
+    alert("Question:4 Answer Is InCorrect")
+}
+alert("Your Result is "+score++ +' OUT Of 4')
+}
